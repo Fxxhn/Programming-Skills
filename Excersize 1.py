5041dbef57665357e6c1d5081d88229026438638
MUHAMMAD FARHAN <muhammadfarhan99001@gmail.com>
2024-11-11 02:25:42 +0400

    Add files via upload

---
+++ Excersize 1.py
@@ -0,0 +1,8 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> word1 = "Coding"
+>>> word2 = "is"
+>>> word3 = "Cool"
+>>> # Use string concatenation to combine the variables and print the phrase
+>>> print(word1 + " " + word2 + " " + word3)
+Coding is Cool
---
+++ Excersize 2.py
@@ -0,0 +1,7 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> num1 = 8
+>>> num2 = 10
+>>> sum_result = num1 + num2
+>>> print(sum_result)
+18
